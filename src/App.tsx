21c7ce9511390dca8c877f35da7a559c19d6f891
Muhamad Irgi fahrezi <muhamad.irgi@indocyber.id>
2025-02-03 10:21:53 +0700

    ee

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
 import { useEffect, useState } from "react";
+import PhoneInput from "./PhoneInput";
 
 function App() {
   const [phoneNo, setPhoneNo] = useState("");
   const [label, setLabel] = useState("Phone");
   const [status, setStatus] = useState("xxx");
-
-  async function getContact() {
-    if (!("contacts" in navigator && "ContactsManager" in window)) {
-      alert("Contacts API not supported on this browser.");
-      return;
-    }
-
-    try {
-      const props = ["name", "tel"];
-      const opts = { multiple: false };
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const contacts = await (navigator.contacts as any).select(props, opts);
-
-      if (contacts.length > 0) {
-        const tel = String(contacts[0].tel).replace(/[^a-zA-Z0-9]/g, "");
-
-        if (tel.startsWith("62")) {
-          setPhoneNo(tel.replace("62", "0"));
-        } else {
-          setPhoneNo(tel);
-        }
-
-        setLabel(contacts[0].name);
-      }
-    } catch (error) {
-      alert(`Error fetching contact: ${error}`);
-    }
-  }
 
   useEffect(() => {
     setStatus("number is " + phoneNo);
@@ -39,9 +12,12 @@ function App() {
 
   return (
     <div>
-      <label htmlFor="phone">{label}</label>
-      <input type="tel" value={phoneNo} onChange={(e) => setPhoneNo(e.target.value)} />
-      <button onClick={getContact}>Get Phone</button>
+      <PhoneInput
+        label={label}
+        phoneNo={phoneNo}
+        setLabel={(e) => setLabel(e)}
+        setPhoneNo={(e) => setPhoneNo(e)}
+      />
       <p>{status}</p>
     </div>
   );
